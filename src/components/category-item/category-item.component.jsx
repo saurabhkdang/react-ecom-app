112df3674613f0c97c4d8e183b5d41de971ec246
Saurabh Dang <saurabh.dang@technoscore.net>
2022-12-08 13:15:38 +0530

    Directory & Category Item Component with initial code

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,15 @@
+import './categories.styles.scss'
+
+const CategoryItem = ({category}) =>{
+    const {title, imageUrl} = category
+    return (
+        <div className="category-container" style={{backgroundImage:`url(${imageUrl})`}}>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        )
+}
+
+export default CategoryItem;
