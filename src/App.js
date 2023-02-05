dd4fd0095d3b169b6a6e0d4253e1071c58fcb7e6
Saurabh Dang <saurabh.dang@technoscore.net>
2023-02-05 21:01:16 +0530

    pwa changes, converted few components into jsx, some media queries for responsiveness

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useEffect, lazy, Suspense } from "react";
 import { checkUserSession } from './store/user/user.action';
 import Spinner from './components/spinner/spinner.component';
+import { GlobalStyle } from './global.styles';
 
 const Home = lazy(() => import('./routes/home/home.component'));
 const Navigation = lazy(() => import('./routes/navigation/navigation.component'));
@@ -20,6 +21,7 @@ const App = ()=>{
 
   return (
     <Suspense fallback={<Spinner/>}>
+      <GlobalStyle/>
       <Routes>
         <Route path='/' element={<Navigation/>}>
           <Route index element={<Home/>} />
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
@@ -9,6 +9,7 @@ import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Elements } from '@stripe/react-stripe-js';
 import { stripePromise } from './utils/stripe/stripe.utils';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -29,3 +30,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
